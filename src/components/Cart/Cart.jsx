3197ca4c7821a12baeb06b32913e2f5a2bc5362a
Yoel Diomedez <yoeldiomedez@gmail.com>
2021-05-20 18:20:18 -0500

    Cart Buttons

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Container, Typography, Button, Grid } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { Container, Typography, Button, Grid } from '@material-ui/core';
 
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
     const classes = useStyles();
 
     const EmptyCart = () => (
-        <Typography variant="subtitle1">You have no items in your shopping cart,
-            <Link to="/" className={classes.link}>start adding some!</Link>
+        <Typography variant="subtitle1">
+            You have no items in your shopping cart, <Link to="/" className={classes.link}>start adding some!</Link>
         </Typography>
     )
 
@@ -19,15 +19,22 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <CartItem item={item} />
+                        <CartItem item={item} 
+                            onUpdateCartQty={handleUpdateCartQty}
+                            onRemoveFromCart={handleRemoveFromCart}
+                        />
                     </Grid>
                 ))}
             </Grid>
             <div className={classes.cardDetails}>
                 <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
                 <div>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
-                    <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>
+                        Empty Cart
+                    </Button>
+                    <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">
+                        Checkout
+                    </Button>
                 </div>
             </div>
         </>
